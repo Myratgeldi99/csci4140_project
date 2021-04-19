8ae41e93d181498565254d2c4e6b1dfa2ea72436
Myratgeldi99 <myratgeldi.j.99@gmail.com>
2021-04-19 23:33:52 +0800

    route fix

--- index.js
+++ index.js
@@ -24,8 +24,11 @@ app.use((req, res, next) => {
 app.use('/api/users', users);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 }
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 
 
 const port = process.env.PORT || 5000;
