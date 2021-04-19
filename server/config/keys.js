adfbf6bd87aa18a807d58412239aecc079bb27d2
Myratgeldi99 <myratgeldi.j.99@gmail.com>
2021-04-19 21:08:32 +0800

    heroku

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://csci4140_project:GrOWBUa5fOGNtOCP@cluster0.i1qwe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    secretOrKey: "csci4140_project"
-};
